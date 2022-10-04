d092679353967121c70c1dcd33835fa8f4694942
devjiin <2ddung@naver.com>
2022-10-04 20:47:09 +0900

    #3.5 Forms

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+	const [value, setValue] = useState("");
+	const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+		const {
+			currentTarget: { value },
+		} = event;
+	
+	setValue(value);
+	};
+	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		console.log('hi ' + value);
+	}
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow" text="im option"/>
-      <Circle bgColor="tomato" />
+		<form onSubmit={onSubmit}>
+			<input value={value} onChange={onChange} type="text" placeholder="username" />
+			<button>Log in</button>
+		</form>
     </div>
   );
 }
