5326d619c6d6e7e10634187483e719cbbbfeb861
devjiin <2ddung@naver.com>
2022-12-11 15:28:01 +0900

    #5.12 Price Chart

--- react-typescript/src/api.ts
+++ react-typescript/src/api.ts
@@ -11,3 +11,9 @@ export function fetchCoinInfo(coinID: string) {
 export function fetchCoinTickers(coinID: string) {
 	return fetch(`${BASE_URL}/tickers/${coinID}`).then((response) => response.json());
 }
+
+export function fetchCoinHistory(coinID: string) {
+	const endDate = Math.floor(Date.now() / 1000); //니꼬쌤 자체 개발 api 에서는 endData, startDate 사용 X 
+	const startData = endDate - 60 * 60 * 24 * 14;
+	return fetch(`https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinID}`).then((response) => response.json());
+}
--- react-typescript/src/routes/Coin.tsx
+++ react-typescript/src/routes/Coin.tsx
@@ -201,7 +201,7 @@ function Coin() {
 							<Price />
 						</Route>
 						<Route path={`/:coinId/chart`}>
-							<Chart />
+							<Chart coinId={coinId} />
 						</Route>
 					</Switch>
 				</>
