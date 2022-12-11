877fc7be101414d7f6a0e04f9bcca5c7d904508e
devjiin <2ddung@naver.com>
2022-12-11 16:48:38 +0900

    #5.13 Price Chart part Two

--- react-typescript/src/routes/Chart.tsx
+++ react-typescript/src/routes/Chart.tsx
@@ -1,12 +1,48 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
-interface ChartProps {
-	coinId: string,
+interface Ihistorical {
+	time_open: number;
+	time_close: number;
+	open: string;
+	high: string;
+	low: string;
+	close: string;
+	volume: string;
+	market_cap: number;
 }
-function Chart({ coinId } : ChartProps){
-	const {isLoading, data} = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId))
-	return <h1>Chart</h1>
+interface ChartProps {
+	coinId: string;
+}
+function Chart({ coinId }: ChartProps) {
+	const { isLoading, data } = useQuery<Ihistorical[]>(["ohlcv", coinId], () =>
+		fetchCoinHistory(coinId)
+	);
+	// const 
+	return (
+		<div>
+			{isLoading ? (
+				"loading chart..."
+			) : (
+				<ApexChart
+					type="line"
+					series= {[
+						{ name: "price", data: data?.map((price) => parseFloat(price.close)) ?? []}, //니꼬쌤 제공 api는 강의와 달라 close값이 string이기 때문에 정수로 변환해줘야 한다. (?? null 병합 연산자)
+					]}
+					options={{
+						
+						theme: { mode: "dark" },
+						chart: { height: 500, width: 500, toolbar: {show: false}, background: 'transparent' },
+						grid: {show: false},
+						stroke: {curve: "smooth", width: 4},
+						yaxis: {show: false},
+						xaxis: {labels: {show: false}, axisTicks:{show: false}, axisBorder: {show: false}}
+					}}
+				/>
+			)}
+		</div>
+	);
 }
 
 export default Chart;
